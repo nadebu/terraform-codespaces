c69d5a0e074caf9cd225222dedc050c95fe99d15
Joel Mulimba <46745130+nadebu@users.noreply.github.com>
2026-03-06 11:02:04 +0000

    lab 4 completion

--- labs/terraform/terraform.tfvars
+++ labs/terraform/terraform.tfvars
@@ -1,8 +1,14 @@
-repository_name     = "terraform-development"
+repository_name       = "terraform-development"
 repository_visibility = "public"
-environment        = "development"
+environment           = "development"
 repository_features = {
   has_issues      = true
   has_discussions = false
   has_wiki        = false
 }
+
+# Development Repo Configurations
+dev_repository_name = "development-repo"
+dev_repo_issues     = true
+dev_wiki            = true
+dev_discussions     = false
--- labs/terraform/variables.tf
+++ labs/terraform/variables.tf
@@ -18,7 +18,7 @@ variable "environment" {
 
 variable "repository_features" {
   description = "Enabled features for the repository"
-  type        = object({
+  type = object({
     has_issues      = bool
     has_discussions = bool
     has_wiki        = bool
@@ -28,4 +28,29 @@ variable "repository_features" {
     has_discussions = true
     has_wiki        = false
   }
+}
+
+# Development Repo Variables
+variable "dev_repository_name" {
+  description = "Name of the Dev GitHub repository"
+  type        = string
+  default     = "development-repo"
+}
+
+variable "dev_repo_issues" {
+  description = "Dev repo issues settings"
+  type        = bool
+  default     = true
+}
+
+variable "dev_discussions" {
+  description = "Dev repo discussions settings"
+  type        = bool
+  default     = true
+}
+
+variable "dev_wiki" {
+  description = "Dev repo wiki settings"
+  type        = bool
+  default     = true
 }
